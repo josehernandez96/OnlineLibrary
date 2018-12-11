5210ffbdd61e926ff459653502ceaf2a83445ade
Jose Hernandez <44278721+josehernandez96@users.noreply.github.com>
2018-12-10 19:32:28 -0500

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,165 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Hate U Give",
+	"img" => "img/media/hate.jpg",
+    "genre" => "fiction",
+    "format" => "Paperback",
+    "year" => 2017,
+    "category" => "Books",
+    "authors" => [
+        "Angie Thomas",
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+$catalog[102] = [
+	"title" => "We Are All Completely Beside Ourselves",
+ "img" => "img/media/beside.jpg",
+    "genre" => "fiction",
+    "format" => "Ebook",
+    "year" => 2014,
+    "category" => "Books",
+    "authors" => [
+        "Karen Joy Fowler",
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0132350884'
+];
+$catalog[103] = [
+		"title" => "The Dogist",
+		"img" => "img/media/dogist.jpg",
+		"genre" => "Photobook",
+		"format" => "Hardcover",
+    "year" => 2015,
+    "category" => "Books",
+    "authors" => [
+        "Elias Weiss Friedman",
+    ],
+    "publisher" => "Addison-Wesley Professional",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+		"title" => "Hyrule Historia",
+		"img" => "img/media/historia.jpg",
+		"genre" => "Enclycopedia",
+		"format" => "Hardcover",
+    "year" => 2013,
+    "category" => "Books",
+    "authors" => [
+        "Dark Horse"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+		"title" => "Interstellar",
+		"img" => "img/media/interstellar.jpg",
+		"genre" => "Action,Adventure,Fantasy",
+		"format" => "DVD",
+		"year" => 2014,
+		"category" => "Movies",
+		"director" => "Christopher Nolan",
+    "writers" => [
+        "Winston Groom",
+        "Eric Roth"
+    ],
+		"stars" => [
+      "Matthew McConaughey",
+      "Anne Hathaway",
+      "Jessica Chastain"
+    ]
+];
+$catalog[202] = [
+		"title" => "Avengers",
+		"img" => "img/media/avengers.jpg",
+		"genre" => "Action,Adventure",
+		"format" => "DVD",
+		"year" => 2012,
+		"category" => "Movies",
+		"director" => "Joss Whedon",
+    "writers" => [
+        "William Goldman"
+    ],
+		"stars" => [
+      "Robert Downey Jr",
+      "Scarlett Johanssen",
+      "Chris Evans"
+    ]
+];
+$catalog[203] = [
+		"title" => "The Martian",
+		"img" => "img/media/martian.jpg",
+		"genre" => "Action,SciFi",
+		"format" => "DVD",
+		"year" => 2015,
+		"category" => "Movies",
+		"director" => "Ridley Scott",
+    "writers" => [
+        "J.R.R. Tolkien",
+        "Fran Walsh",
+        "Philippa Boyens",
+        "Peter Jackson"
+    ],
+    "stars" => [
+			"Matt Damon",
+			"Jessica Chastain",
+			"Chiwetel Ejifor"
+    ]
+];
+$catalog[204] = [
+		"title" => "Arrival",
+		"img" => "img/media/arrival.jpg",
+		"genre" => "Action,SciFi",
+		"format" => "DVD",
+		"year" => 2016,
+		"category" => "Movies",
+		"director" => "Denis Villeneuve",
+    "writers" => [
+        "William Goldman"
+    ],
+		"stars" => [
+      "Amy Adams",
+      "Jeremy Renner Saldana",
+      "Forest Whitaker"
+    ]
+];
+//Music
+$catalog[301] = [
+	"title" => "Mylo Xyloto",
+	"img" => "img/media/coldplay.jpg",
+	"genre" => "Alternative",
+	"format" => "CD",
+	"year" => 2011,
+	"category" => "Music",
+	"artist" => "Coldplay"
+];
+$catalog[302] = [
+	"title" => "Weezer: Red Album",
+	"img" => "img/media/weezer.jpg",
+	"genre" => "Alternative",
+	"format" => "CD",
+	"year" => 2008,
+	"category" => "Music",
+	"artist" => "Weezer"
+];
+$catalog[303] = [
+	"title" => "X&Y",
+  "img" => "img/media/xy.jpg",
+  "genre" => "Alternative",
+  "format" => "CD",
+  "year" => 2005,
+  "category" => "Music",
+  "artist" => "Coldplay"
+];
+$catalog[304] = [
+	"title" => "1000 Forms of Fear",
+	  "img" => "img/media/sia.png",
+	  "genre" => "Pop",
+	  "format" => "CD",
+	  "year" => 2014,
+	  "category" => "Music",
+	  "artist" => "Sia"
+];
---
+++ My Online Library/inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>
+				<li><a href="http://twitter.com/DJ4Mile">Twitter</a></li>
+				<li><a href="https://www.instagram.com/DJ4Mile">Instagram</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> My Online Library</p>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
