ff518c2c70bad9403e0e5a77aab4b53e1d17d2a1
Jose Hernandez <44278721+josehernandez96@users.noreply.github.com>
2018-12-16 18:17:06 -0500

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,161 @@
+<?php
+
+*//Books*
+$catalog[101] = [
+   "title" => "The Hate U Give",
+   "img" => "img/media/hate.jpg",
+  "genre" => "fiction",
+  "format" => "Paperback",
+  "year" => 2017,
+  "category" => "Books",
+  "authors" =>
+      "Angie Thomas",
+
+  "isbn" => '9780399185045'
+];
+
+$catalog[102] = [
+   "title" => "We Are All Completely Beside Ourselves",
+   "img" => "img/media/beside.jpg",
+  "genre" => "fiction",
+  "format" => "Paperback",
+  "year" => 2014,
+  "category" => "Books",
+  "authors" =>
+      "Karen Joy Fowler",
+
+  "isbn" => '9780399185045'
+];
+
+$catalog[103] = [
+   "title" => "The Dogist",
+   "img" => "img/media/dogist.jpg",
+  "genre" => "Photobook",
+  "format" => "Hardcover",
+  "year" => 2015,
+  "category" => "Books",
+  "authors" =>
+      "Elias Weiss Friedman",
+
+  "isbn" => '9780399185045'
+];
+
+$catalog[104] = [
+   "title" => "Hyrule Historia",
+   "img" => "img/media/historia.jpg",
+  "genre" => "Enclycopedia",
+  "format" => "Hardcover",
+  "year" => 2013,
+  "category" => "Books",
+  "authors" => [
+      "Nintendo",
+
+  "isbn" => '9780399185045'
+];
+
+*//Movies*
+$catalog[201] = [
+  "title" => "Interstellar",
+  "img" => "img/media/interstellar.jpg",
+  "genre" => "Action,Adventure,Fantasy",
+  "format" => "DVD",
+  "year" => 2014,
+  "category" => "Movies",
+  "director" => "Christopher Nolan",
+  "stars" => [
+      "Matthew McConaughey",
+      "Anne Hathaway",
+      "Jessica Chastain"
+  ]
+];
+
+$catalog[202] = [
+  "title" => "Avengers",
+  "img" => "img/media/avengers.jpg",
+  "genre" => "Action,Adventure",
+  "format" => "DVD",
+  "year" => 2012,
+  "category" => "Movies",
+  "director" => "Joss Whedon",
+
+  "stars" => [
+      "Robert Downey Jr",
+      "Scarlett Johanssen",
+      "Chris Evans"
+  ]
+];
+
+$catalog[203] = [
+  "title" => "The Martian",
+  "img" => "img/media/martian.jpg",
+  "genre" => "Action,SciFi",
+  "format" => "DVD",
+  "year" => 2015,
+  "category" => "Movies",
+  "director" => "Ridley Scott",
+
+  "stars" => [
+      "Matt Damon",
+      "Jessica Chastain",
+      "Chiwetel Ejifor"
+  ]
+];
+
+$catalog[204] = [
+  "title" => "Arrival",
+  "img" => "img/media/arrival.jpg",
+  "genre" => "Action,SciFi",
+  "format" => "DVD",
+  "year" => 2016,
+  "category" => "Movies",
+  "director" => "Denis Villeneuve",
+
+  "stars" => [
+      "Amy Adams",
+      "Jeremy Renner Saldana",
+      "Forest Whitaker"
+  ]
+];
+
+*//Music*
+$catalog[301] = [
+  "title" => "Mylo Xyloto",
+  "img" => "img/media/coldplay.jpg",
+  "genre" => "Alternative",
+  "format" => "CD",
+  "year" => 2011,
+  "category" => "Music",
+  "artist" => "Coldplay"
+];
+
+$catalog[302] = [
+  "title" => "Weezer: Red Album",
+  "img" => "img/media/weezer.jpg",
+  "genre" => "Alternative",
+  "format" => "CD",
+  "year" => 2008,
+  "category" => "Music",
+  "artist" => "Weezer"
+];
+
+$catalog[303] = [
+  "title" => "X&Y",
+  "img" => "img/media/xy.jpg",
+  "genre" => "Alternative",
+  "format" => "CD",
+  "year" => 2005,
+  "category" => "Music",
+  "artist" => "Coldplay"
+];
+
+$catalog[304] = [
+  "title" => "1000 Forms of Fear",
+  "img" => "img/media/sia.jpg",
+  "genre" => "Clasical",
+  "format" => "CD",
+  "year" => 2012,
+  "category" => "Music",
+  "artist" => "Ludwig van Beethoven"
+];
+
+?>
